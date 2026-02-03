d8f1b548ad3e2055a235f2e1a5f42f3244ab75af
Gheric Santiago <ghericsantiago@gmail.com>
2026-02-03 14:34:25 +0800

    abbreviate numbers on best items to buy

--- script.js
+++ script.js
@@ -1041,6 +1041,20 @@ async function fetchBestItems() {
 
     DOM_ELEMENTS.bestItemsList.innerHTML = "";
 
+    // Function to format large numbers with abbreviations
+    const formatLargeNumber = (num) => {
+      const number = parseFloat(num);
+      if (number >= 1000000000000) {
+        return "$" + (number / 1000000000000).toFixed(1) + "t";
+      } else if (number >= 1000000) {
+        return "$" + (number / 1000000).toFixed(1) + "m";
+      } else if (number >= 1000) {
+        return "$" + (number / 1000).toFixed(1) + "k";
+      } else {
+        return "$" + number.toFixed(2);
+      }
+    };
+
     data.forEach((item) => {
       const formatPrice = (price) => {
         return (
@@ -1059,10 +1073,10 @@ async function fetchBestItems() {
           <div class="best-item-name">${item.name}</div>
           <div class="best-item-id">ID: ${item.item_id}</div>
         </div>
-        <div class="best-item-price">${formatPrice(
+        <div class="best-item-price">${formatLargeNumber(
           item.avg_actual_price_last_week
         )}</div>
-        <div class="best-item-profit">${formatPrice(item.margin)}</div>
+        <div class="best-item-profit">${formatLargeNumber(item.margin)}</div>
         <div class="best-item-margin">+${item.margin_percent}%</div>
       `;
 
